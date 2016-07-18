e162b4eb8a2201f0b0b2975cbc8d728cd409861c
indeep-xyz <indeep.xyz@gmail.com>
2016-07-18 15:51:18 +0900

    Refactor spec for InsensitiveLoad::Collector
    spec for InsensitiveLoad::Collector
    ----
    
    - Add a describe for the getter methods.
      - Contain specs for #items, #dirs, #files and shared examples only used by them.

--- spec/lib/insensitive_load/collector_spec.rb
+++ spec/lib/insensitive_load/collector_spec.rb
@@ -21,157 +21,159 @@ describe InsensitiveLoad::Collector do
     end
   end
 
-  shared_examples 'return path equal insensitively the path source' do |method_name|
-    let(:returner) { instance.send(method_name).map(&:downcase) }
+  describe 'methods to get data in @collection' do
+    shared_examples 'return path equal insensitively the path source' do |method_name|
+      let(:returner) { instance.send(method_name).map(&:downcase) }
 
-    it 'return the equivalent insensitively' do
-      expect(returner).to \
-          all(eq(path_src.downcase))
-    end
-  end
-
-  shared_examples 'initialized by absolute path' do |method_name|
-    context 'without "absolute_path" option' do
-      it_behaves_like \
-          'return path equal insensitively the path source',
-          method_name
-    end
-
-    context 'with "absolute_path" option' do
-      let(:returner) { instance.send(method_name) }
-      let(:returner_absoluted) { instance.send(method_name, true) }
-
-      it 'return an array including absolute path' do
-        expect(returner_absoluted).to \
-            eq(returner)
+      it 'return the equivalent insensitively' do
+        expect(returner).to \
+            all(eq(path_src.downcase))
       end
     end
-  end
 
-  shared_examples 'initialized by relative path' do |method_name|
-    context 'without "absolute_path" option' do
-      it_behaves_like \
-          'return path equal insensitively the path source',
-          method_name
-    end
-
-    context 'with "absolute_path" option' do
-      let(:returner) { instance.send(method_name) }
-      let(:returner_absoluted) { instance.send(method_name, true) }
-
-      it 'return an array including absolute path' do
-        returner_absoluted.each do |path|
-          matched_head = path.downcase.index(path_src.downcase)
-          expect(matched_head).to \
-              be > 1
-        end
+    shared_examples 'initialized by absolute path' do |method_name|
+      context 'without "absolute_path" option' do
+        it_behaves_like \
+            'return path equal insensitively the path source',
+            method_name
       end
-    end
-  end
 
-  describe '#items' do
-    subject { instance.items }
+      context 'with "absolute_path" option' do
+        let(:returner) { instance.send(method_name) }
+        let(:returner_absoluted) { instance.send(method_name, true) }
 
-    context 'when passed a relative path of existence' do
-      let(:path_src) { 'ext/soFTwaRe/Config.conf' }
-
-      include_context \
-          'at root of the sample file structure'
-
-      it_behaves_like \
-          'to return an instance of Array',
-          4
-
-      it_behaves_like \
-          'initialized by relative path',
-          :items
-
-      context 'with unreasonable "delimiter" option' do
-        let(:instance) {
-          described_class.new(
-              path_src,
-              delimiter: 'DELIMITER_STRING')
-        }
-
-        it 'return an empty array' do
-          expect(subject.size).to \
-              eq(0)
+        it 'return an array including absolute path' do
+          expect(returner_absoluted).to \
+              eq(returner)
         end
       end
     end
 
-    context 'when passed an absolute path of existence' do
-      let(:path_src) { '/eTc/HOsTS' }
+    shared_examples 'initialized by relative path' do |method_name|
+      context 'without "absolute_path" option' do
+        it_behaves_like \
+            'return path equal insensitively the path source',
+            method_name
+      end
 
-      it_behaves_like \
-          'to return an instance of Array',
-          1
+      context 'with "absolute_path" option' do
+        let(:returner) { instance.send(method_name) }
+        let(:returner_absoluted) { instance.send(method_name, true) }
 
-      it_behaves_like \
-          'initialized by absolute path',
-          :items
-    end
-  end
-
-  describe '#dirs' do
-    subject { instance.dirs }
-
-    context 'when passed a relative path of existence' do
-      let(:path_src) { 'ext/soFTwaRe/Config.conf' }
-
-      include_context \
-          'at root of the sample file structure'
-
-      it_behaves_like \
-          'to return an instance of Array',
-          1
-
-      it_behaves_like \
-          'initialized by relative path',
-          :dirs
+        it 'return an array including absolute path' do
+          returner_absoluted.each do |path|
+            matched_head = path.downcase.index(path_src.downcase)
+            expect(matched_head).to \
+                be > 1
+          end
+        end
+      end
     end
 
-    context 'when passed an absolute path of existence' do
-      let(:path_src) { '/uSR/Bin' }
+    describe '#items' do
+      subject { instance.items }
 
-      it_behaves_like \
-          'to return an instance of Array',
-          1
+      context 'when passed a relative path of existence' do
+        let(:path_src) { 'ext/soFTwaRe/Config.conf' }
 
-      it_behaves_like \
-          'initialized by absolute path',
-          :dirs
+        include_context \
+            'at root of the sample file structure'
+
+        it_behaves_like \
+            'to return an instance of Array',
+            4
+
+        it_behaves_like \
+            'initialized by relative path',
+            :items
+
+        context 'with unreasonable "delimiter" option' do
+          let(:instance) {
+            described_class.new(
+                path_src,
+                delimiter: 'DELIMITER_STRING')
+          }
+
+          it 'return an empty array' do
+            expect(subject.size).to \
+                eq(0)
+          end
+        end
+      end
+
+      context 'when passed an absolute path of existence' do
+        let(:path_src) { '/eTc/HOsTS' }
+
+        it_behaves_like \
+            'to return an instance of Array',
+            1
+
+        it_behaves_like \
+            'initialized by absolute path',
+            :items
+      end
     end
-  end
 
-  describe '#files' do
-    subject { instance.files }
+    describe '#dirs' do
+      subject { instance.dirs }
 
-    context 'when passed a relative path of existence' do
-      let(:path_src) { 'ext/soFTwaRe/Config.conf' }
+      context 'when passed a relative path of existence' do
+        let(:path_src) { 'ext/soFTwaRe/Config.conf' }
 
-      include_context \
-          'at root of the sample file structure'
+        include_context \
+            'at root of the sample file structure'
 
-      it_behaves_like \
-          'to return an instance of Array',
-          3
+        it_behaves_like \
+            'to return an instance of Array',
+            1
 
-      it_behaves_like \
-          'initialized by relative path',
-          :files
+        it_behaves_like \
+            'initialized by relative path',
+            :dirs
+      end
+
+      context 'when passed an absolute path of existence' do
+        let(:path_src) { '/uSR/Bin' }
+
+        it_behaves_like \
+            'to return an instance of Array',
+            1
+
+        it_behaves_like \
+            'initialized by absolute path',
+            :dirs
+      end
     end
 
-    context 'when passed an absolute path of existence' do
-      let(:path_src) { '/eTc/HOsTS' }
+    describe '#files' do
+      subject { instance.files }
 
-      it_behaves_like \
-          'to return an instance of Array',
-          1
+      context 'when passed a relative path of existence' do
+        let(:path_src) { 'ext/soFTwaRe/Config.conf' }
 
-      it_behaves_like \
-          'initialized by absolute path',
-          :files
+        include_context \
+            'at root of the sample file structure'
+
+        it_behaves_like \
+            'to return an instance of Array',
+            3
+
+        it_behaves_like \
+            'initialized by relative path',
+            :files
+      end
+
+      context 'when passed an absolute path of existence' do
+        let(:path_src) { '/eTc/HOsTS' }
+
+        it_behaves_like \
+            'to return an instance of Array',
+            1
+
+        it_behaves_like \
+            'initialized by absolute path',
+            :files
+      end
     end
   end
 end
