e370e8c96eab61d622d219e3b5312fc0c2fe6ec8
indeep-xyz <indeep.xyz@gmail.com>
2016-07-18 16:40:55 +0900

    Add spec for InsensitiveLoad
    spec for InsensitiveLoad
    ----
    
    - Add a describe for the getter methods.
      - Add specs for .items, .dirs, .files and shared examples used by them.
    - The shared example 'to use the same name method in Collector'.
      - It is a brief version because the main spec for InsensitiveLoad::Collector is managed in another file.

--- spec/lib/insensitive_load_spec.rb
+++ spec/lib/insensitive_load_spec.rb
@@ -4,4 +4,36 @@ describe InsensitiveLoad do
   it 'has a version number' do
     expect(InsensitiveLoad::VERSION).not_to be nil
   end
+
+  describe 'methods to get pathes' do
+    shared_examples 'to use the same name method in Collector' do |method_name|
+      subject { described_class.send(method_name, path_src) }
+      let(:path_src) { 'PATH/TO/UNREASONABLE_' + [].__id__.to_s }
+
+      context 'when passed a unreasonable path source' do
+        it 'return an instance of Array' do
+          expect(subject).to \
+              be_a_kind_of(Array)
+        end
+      end
+    end
+
+    describe '.items' do
+      it_behaves_like \
+          'to use the same name method in Collector',
+          :items
+    end
+
+    describe '.dirs' do
+      it_behaves_like \
+          'to use the same name method in Collector',
+          :dirs
+    end
+
+    describe '.files' do
+      it_behaves_like \
+          'to use the same name method in Collector',
+          :files
+    end
+  end
 end
