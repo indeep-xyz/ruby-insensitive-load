f678f2417e6237131c28bc6ef742e65f5072b635
indeep-xyz <indeep.xyz@gmail.com>
2016-08-13 15:49:36 +0900

    Move the directory used for testing relative path
    /spec/sample
    ----
    
    - Move to /spec/common/relative_root.
    
    /spec/common/relative_path.rb
    ----
    
    - Fix the shared context 'relative path in the sample file structure'.
      - Change to the newer path.

--- spec/common/relative_path.rb
+++ spec/common/relative_path.rb
@@ -1,6 +1,6 @@
 shared_context 'relative path in the sample file structure' do
   before {
-    Dir.chdir(File.expand_path('../../sample', __FILE__))
+    Dir.chdir(File.expand_path('../relative_root', __FILE__))
   }
 
   let(:path_source) { 'ext/soFTwaRe/Config.conf' }
