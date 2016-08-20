0d87ab1a08deab0db7863afdbcae997b7afb3796
indeep-xyz <indeep.xyz@gmail.com>
2016-08-20 23:03:40 +0900

    Add YARD comment to Engine::Windows#search_start
    InsensitiveSearch::Engine::Windows
    ----
    
    - Add YARD comment to #search_start.

--- lib/insensitive_search/engine/windows.rb
+++ lib/insensitive_search/engine/windows.rb
@@ -5,6 +5,16 @@ module InsensitiveSearch
     class Windows < Base
       private
 
+      # Start searching.
+      #
+      # The return value has been just
+      # checked existence at the argument path
+      # and filtered.
+      # On the Windows environment,
+      # its way of resolving path is insensitive.
+      #
+      # @param [String] path the path
+      # @return [Array] matched and filtered paths
       def search_start(path)
         File.exist?(path) \
             && @filter.ok?(path) ? [path] : []
