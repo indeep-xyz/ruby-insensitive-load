5547a63c19b916e37c8ff9c76ed1e67860134764
indeep-xyz <indeep.xyz@gmail.com>
2016-08-21 19:23:40 +0900

    Add YARD comment to Engine::Linux#search_loop
    InsensitiveSearch::Engine::Linux
    ----
    
    - Add YARD comment to #search_loop.

--- lib/insensitive_search/engine/linux.rb
+++ lib/insensitive_search/engine/linux.rb
@@ -20,6 +20,22 @@ module InsensitiveSearch
             : search_loop(parts)
       end
 
+      # Search recursively.
+      #
+      # @note the processing flow
+      #   1-1. Start the first loop with the arguments, ['path', 'to', 'filename'], ''.
+      #   1-2. Call itself with the arguments, ['to', 'filename'], 'path'.
+      #   2-1. Start with the arguments from 1-2.
+      #   2-2. Call itself with the arguments, ['filename'], 'path/to'.
+      #   3-1. Start with the arguments from 2-2.
+      #   3-2. Add path to the return value if the path 'path/to/filename' is found insensitively and passed through the filter.
+      #   3-3. Return value.
+      #   2-3. Return the value from 3-3.
+      #   1-3. Return the value from 2-3.
+      #
+      # @param [Array] parts the path split by directory separator on Linux.
+      # @param [String] prefix the prefix of path
+      # @return [Array] matched and filtered paths
       def search_loop(parts, prefix = '')
         list = search_in_dir(prefix, parts[0])
 
